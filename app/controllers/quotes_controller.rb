80f37111586d6d07422a7515ed335e5287c995f8
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-04 01:44:36 +0000

    Revert "test"
    This reverts commit 7bbc9b32cf8c709a854e72539e1af9a953664197.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,19 +1,19 @@
 class QuotesController < ApplicationController
   
   def index 
-    @quote = Quote.order(RANDOM()).first
+  @quote = Quote.order("RANDOM()").first
   end
 
   def create
-    @quote = Quote.create(quote_params)
-    if @quote.invalid?
-      flash[:error] = '<strong>Could not save</strong>, bacause the data you have entered is invalid.'
-    end
-    redirect_to root_path
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong>, bacause the data you have entered is invalid.'
   end
+  redirect_to root_path
+end
 
-  def about
-  end
+def about
+end
 
   private
 
